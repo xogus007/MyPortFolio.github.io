a04055ea7848265d659f05feec4dbeab83ad3e3d
xogus007 <121161535+xogus007@users.noreply.github.com>
2023-06-21 09:40:48 +0900

    Update ConstKey.java

--- Android_studio_java/app/src/main/java/com/lkw/searchbar/unlogin/key/ConstKey.java
+++ Android_studio_java/app/src/main/java/com/lkw/searchbar/unlogin/key/ConstKey.java
@@ -2,7 +2,7 @@ package com.lkw.searchbar.unlogin.key;
 
 public class ConstKey {
     public static final String RETROFIT_KAKAO_BASE_URL = "https://dapi.kakao.com/";
-    public static final String KAKAO_MAP_REST_API_APP_KEY = "(Kakao Developers REST API 키)";
+    public static final String KAKAO_MAP_REST_API_APP_KEY = " (Kakao Developers REST API 키) ";
 
 }
 
